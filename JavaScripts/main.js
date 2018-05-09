aad4d8c0ae05f6aeff7978cfc09b69856018b299
Amanda Mitchell <wreedamitchell@gmail.com>
2018-05-09 18:39:19 -0500

    domstring printing

--- JavaScripts/main.js
+++ JavaScripts/main.js
@@ -1,57 +1,73 @@
-// const addEventListeners => {
-//     get Button
-//     button.addEventListeners (e) {
-//         get Player1
-//         get Player 2
-//         genericXHRrequest(player1)
-//         genericXHRrequest(player2)
-//     }
-// }
+const getPlayer1 = () => {
+    let myRequest = new XMLHttpRequest();
+    let player1 = document.getElementById("player1-name").value;
+    myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+    myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+    myRequest.open("GET", `https://teamtreehouse.com/${player1}.json`);
+    myRequest.send();
+    addEventListener();
+  };
 
-// const genericXHRrequest(player) => {
-//     XHR Stuff
-//     request.open("GET", treehouse/player.json)
-    //    genericXHRrequest.addeventlistener (load, successFunction);
-// }
-
-// function successfunction(playerdata) {
-//     const data = JSON.parse(this.responsetext)
-//     buildDomString(data)
-// }
-
-const buildDomString = (data) => {
-    let domString ='';
-}
-
-console.log();
-
-
-const cageMatch = (e) => {
-    // get player usernames
-    const player1name = document.getElementById("player1-name");
-    const player2name = document.getElementById("player2-name");
-
-    // get profile info for each player
-    genericHeroRequest(loadFileForSingleHero);
-
-    //get and set profile pics for each player
-
-    //get and set score for each player
-
-    //calculate and display winner
-
-};
-
-const addEventListener = () => {
-    const buttons = document.getElementsByClassName("start-match");
-    for (let i=0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", cageMatch);
+let players = [];
+const getPlayer2 = (player1) => {
+    let player2 = document.getElementById("player2-name").value;
+    let secondRequest = new XMLHttpRequest();
+    secondRequest.addEventListener("load", afterPlayer1Loads);
+    secondRequest.open("GET", `https://teamtreehouse.com/${player2}.json`);
+    secondRequest.send();
+    function afterPlayer1Loads(player1) {
+        const player2Data = JSON.parse(this.responseText);
+        players.push(player2Data);
+        buildDomString(players);
     }
+    addEventListener();
 };
 
-addEventListener();
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+};
+
+const buildDomString = (players) => {
+
+    let domString ='';
+    players.forEach((player) => {
+    domString += `<div class="col-md-6">`;
+    domString += `<img src="${player.gravatar_url}" class="player2-image"><br>`;
+    domString += `<span class="score" id="player2-score">${player.points.total}</span>`;
+    domString += `</div>`;
+});
+
+printToDom(domString, "cageMatch");
+cageMatch(players);
+
+};
+const addEventListener = () => {
+    const war = document.getElementById("startMatch");
+    war.addEventListener('click', getPlayer2);
+};
 
 
+const cageMatch = (winnerArray) => {
+    const mate1 = winnerArray[0].points.total;
+    const mate2 = winnerArray[1].points.total;
+        if (mate1 > mate2) {
+          WinnerResults(winnerArray[0]);
+        } else {
+          WinnerResults(winnerArray[1]);
+        }
+};
+
+const WinnerResults = (winner) => {
+  let showWinner = "";
+  showWinner += `<h2><u>${winner.name}</u></h2>`;
+  for (let i = 0; i < winner.badges.length; i++) {
+    showWinner += `<div class="won">
+                              <img class="badges" src="${winner.badges[i].icon_url}" width="50px" height="50px">
+                          </div>`;
+
+  }
+  printToDom(showWinner, 'winner');
+}
 
 
 function executeThisCodeIfXHRFails(){
@@ -63,16 +79,7 @@ function executeThisCodeAfterFileLoaded (){
 //     console.log("this", this);
 //     console.log("this.responseText", this.responseText);
     const data = JSON.parse(this.responseText);
-//     console.log("data", data);
-    // buildDomString(data.superheroes);
+    players.push(data);
 };
 
-const getPlayer = (player) => {
-    let myRequest = new XMLHttpRequest();
-    myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
-    myRequest.addEventListener("error", executeThisCodeIfXHRFails);
-    myRequest.open("GET", `https://teamtreehouse.com/${player}.json`);
-    myRequest.send();
-    // console.log("myrequest", myRequest);
-  };
-  
+getPlayer1();
